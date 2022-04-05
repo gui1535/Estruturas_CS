cbde9f7d17e2575ff22358b9488e5d69f023512f
GUILHERME RODRIGUES ARAUJO <guilherme.araujo1535@gmail.com>
2022-04-05 10:12:06 -0300

    Stack -> Pilhas

--- Estruturas_CS/Program.cs
+++ Estruturas_CS/Program.cs
@@ -32,24 +32,16 @@ namespace Estruturas_CS
 
             //Carro carro = new Carro();
             //carro.Marca = "Honda";
-            //carro.Modelo = "Fit";
-            //carro.Cor = "Vermelho";
+            //carro.Modelo = "ABC";
+            //carro.Cor = "Preto";
             //carro.Placa = "ABC1253";
-            Carro carro2 = new Carro("Ford","ka", "Azul", "EIG1234");
+            Carro carro2 = new Carro("Ford", "ka", "Azul", "EIG1234");
             //Console.WriteLine($"Marca: {carro2.Marca}");
             carro2.ExibirDados();
 
-
-
-
             // Array -> Matrizes
 
-            // Queue -> Filas
-
-            // Stack -> Pilhas
-
-
-            // List - Listas
+            // List - Listas-------------------------------------------------------------------------------------------------------
             List<Carro> carros = new List<Carro>();
             carros.Add(new Carro("Ford", "Ka", "Branco", "BGT7896"));
             carros.Add(new Carro("VW", "Fusca", "Azul", "ADE7985"));
@@ -65,6 +57,38 @@ namespace Estruturas_CS
                 new Carro("Ford", "Focus", "Branco", "BGT7896")
                 ) ? "Tem sim, mano!" : "Tem não, Sangue!";
             Console.WriteLine(teste);
+
+            // Queue -> Filas-------------------------------------------------------------------------------------------------------
+            Queue<string> fila = new Queue<string>();
+            fila.Enqueue("A");
+            fila.Enqueue("B");
+            fila.Enqueue("C");
+
+            Console.WriteLine($"Numeros de pessoas na fila: {fila.Count}"); // Numero de pessoas na fila
+            Console.WriteLine($"Proximo da fila: {fila.Peek()}"); // Proxima pessoa da fila
+            Console.WriteLine($"Numeros de pessoas na fila: {fila.Count}"); // Numero de pessoas na fila
+            Console.WriteLine($"Proximo da fila: {fila.Dequeue()}"); // Chama o proximo 
+            Console.WriteLine($"Numeros de pessoas na fila: {fila.Count}"); // Numero de pessoas na fila
+            Console.WriteLine($"Proximo da fila: {fila.Peek()}"); // Proxima pessoa da fila
+            fila.Enqueue("D");
+            Console.WriteLine($"Cliente acabou de ingressar: {fila.Count}"); // Proxima pessoa da fila
+            Console.WriteLine($"Proximo da fila: {fila.Peek()}"); // Proxima pessoa da fila
+
+            // Stack -> Pilhas-------------------------------------------------------------------------------------------------------
+            Stack<int> pilha = new Stack<int>();
+            pilha.Push(12); // Adiciona elemento na fila
+            pilha.Push(13);
+            pilha.Push(14);
+            pilha.Push(15);
+            //foreach (int item in pilha)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.WriteLine($"Removemos elemento: {pilha.Pop()}"); // Remove primeiro elemento da pilha
+            Console.WriteLine($"Proximo elemento: {pilha.Peek()}");
+
+
+
             Console.ReadKey();
         }
     }
